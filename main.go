a4f29583bc22aa1639d1706e814e9a073c624f4f
Thanakorn <thanakorn.ki@linecorp.com>
2019-01-06 16:19:28 +0700

    add examle 3

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 )
 
+var asset int // for Ex 3
+
 func main() {
+
 	/*
 		This is Channel receipt and send
 		var chOut <-chan int
@@ -12,19 +15,25 @@ func main() {
 	*/
 
 	// Ex 1
-	ch := make(chan struct{})
-	go echo(ch)
-	<-ch
+	// ch := make(chan struct{})
+	// go echo(ch)
+	// <-ch
 
 	// Ex 2
-	chNum := make(chan int)
-	chQ := make(chan struct{})
-	go fibonacci(chNum, chQ)
+	// chNum := make(chan int)
+	// chQ := make(chan struct{})
+	// go fibonacci(chNum, chQ)
 
-	for i := 0; i < 10; i++ {
-		fmt.Println(<-chNum)
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(<-chNum)
+	// }
+	// chQ <- struct{}{}
+
+	// ex 3 Rece condition
+	go setAsset()
+	for i := 0; i < 20; i++ {
+		fmt.Println(asset)
 	}
-	chQ <- struct{}{}
 }
 
 func echo(ch chan struct{}) {
@@ -41,5 +50,11 @@ func fibonacci(chNum chan int, chQ chan struct{}) {
 		case <-chQ:
 			return
 		}
+	}
+}
+
+func setAsset() {
+	for i := 0; i < 100; i++ {
+		asset = 1
 	}
 }
